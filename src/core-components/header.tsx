226228f5ef5f82b62bda44f71e9a014ec3b75b17
levyrodrigues23 <joselevyrodrigues23@gmail.com>
2026-04-23 16:20:23 -0300

    feat: added core-components

---
+++ src/core-components/header.tsx
@@ -0,0 +1,9 @@
+import Container from "../components/container";
+
+export default function Header(){
+    return(
+         <Container as="header" className="mt-3 md:mt-20">
+            
+        </Container>
+    )
+}
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,8 +1,9 @@
 import Container from "../components/container";
+import Logo from "../assets/icons/dev.svg?react"
+
 
 export default function PageHome() {
     return <Container>
-        pagina home
-
-    </Container> 
+        <Logo className=" h-9 md:h-12"/>
+    </Container>
 }
